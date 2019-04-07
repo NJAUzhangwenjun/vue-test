e19203b8ac602094bafda21f8104c7965cd9300e
zhangwenjun <njauzhangwenjun@163.com>
2019-04-08 01:28:21 +0800

    Initial commit

---
+++ Travel/src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    Hello word
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "home.Home",
+    data() {
+      return {}
+    },
+    methods: {}
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue1/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="name" @click="gb" :class="{reds:istrue}">
+    {{name}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "App",
+    data() {
+      return {
+        name: "zhangwenjun",
+        istrue:false
+      }
+    },
+    methods:{
+      gb:function () {
+        this.istrue = !this.istrue;
+        this.name = this.name + 'j';
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .name {
+    padding: 15px;
+  }
+  .reds{
+    color: crimson;
+  }
+
+</style>
